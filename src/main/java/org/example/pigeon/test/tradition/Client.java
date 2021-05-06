66688dbc4f27e4ec8d6c60b0b72f166155ccb478
linhanqing <linhanqing@meituan.com>
2021-05-06 15:09:26 +0800

    callback回调模式

--- src/main/java/org/example/pigeon/test/tradition/Client.java
+++ src/main/java/org/example/pigeon/test/tradition/Client.java
@@ -12,5 +12,10 @@ public class Client {
         EchoService echoService = (EchoService)context.getBean("echoService"); // 获取远程服务代理
         String hello = echoService.echo("world");
         System.out.println( hello );
+
+        // 客户端采用callback回调模式调用服务，客户端将请求提交给pigeon后立马返回，不等待返回结果
+        // pigeon负责接收返回结果并传递给回调对象
+        EchoService echoServiceWithCallback = (EchoService) context.getBean("echoServiceWithCallback");
+        echoServiceWithCallback.echo("world-1");
 	}
 }
---
+++ src/main/java/org/example/pigeon/test/tradition/EchoServiceCallback.java
@@ -0,0 +1,16 @@
+package org.example.pigeon.test.tradition;
+
+import com.dianping.pigeon.remoting.invoker.concurrent.InvocationCallback;
+
+public class EchoServiceCallback implements InvocationCallback {
+
+    @Override
+    public void onSuccess(Object result) {
+        System.out.println("call back: " + result);
+    }
+
+    @Override
+    public void onFailure(Throwable exception) {
+        System.out.println("invoke failure: " + exception.getMessage());
+    }
+}
